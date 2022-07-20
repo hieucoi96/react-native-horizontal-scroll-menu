044beb361e8c7c29ddffc1790ae55524058f4d8a
Hoang Minh Hieu <hieucoi669@gmail.com>
2022-07-20 17:57:41 +0700

    change margin

--- src/index.tsx
+++ src/index.tsx
@@ -170,7 +170,7 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
   },
   buttonStyles: {
-    marginRight: 10,
+    marginHorizontal: 5,
   },
   contentContainerStyle: {
     alignItems: "center",
